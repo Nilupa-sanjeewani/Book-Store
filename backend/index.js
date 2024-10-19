088db30d229ba0e6e40c1ffb66b1f436a6966a44
nilupa786 <129794165+Nilupa-sanjeewani@users.noreply.github.com>
2024-10-19 23:00:30 +0530

    create first http route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from "express";
-import { PORT } from "./config";
+import { PORT } from "./config.js";
+
 
 const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('welcome to MERN Tutorial');
+
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
